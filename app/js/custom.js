fa4fb924d16c22d2da50a688d2d49036caa8fd1a
deryadenizballi <dderyaaballi@gmail.com>
2022-03-16 11:10:35 +0300

    web-ui completed

---
+++ app/js/custom.js
@@ -0,0 +1,10 @@
+// <!-- Initialize Swiper -->
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 5,
+    loop:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
